05ff1854d5c8ad6c79d06621507bc1979d6e6c75
May33n <FlagemyEN@seznam.cz>
2023-03-06 16:44:54 +0100

    idk

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,12 +19,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
+  
   <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
