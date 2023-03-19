9ce6795c0146ed6425f816c1bb3f0b59118a4a84
julisejnohova <julie.sejnohova@gmail.com>
2023-03-19 14:40:22 +0100

    smazany zbytecne soubory, zacinam praci na side nav bar

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import { createRouter, createWebHashHistory } from 'vue-router'  
+import Home from './views/Home.vue'
+
+const router = createRouter
+    history: createWebHashHistory
+    routes: [
+        {
+                path: '/',
+                component: Home
+        }
+    ]
+
 createApp(App).mount('#app')
