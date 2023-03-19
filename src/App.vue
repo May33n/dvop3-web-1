61136780af5153b91c7168b5cdead6f64b4ec6fb
May33n <FlagemyEN@seznam.cz>
2023-03-19 15:08:46 +0100

    magie lol

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,126 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Today from './components/Today.vue'
+import Tommorow from './components/Tommorow.vue';
+
+export default {
+  name: 'App',
+  components: { Today, Tommorow }
+}
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="app">
+    <div class="cont-main">
+      <div class="cont-left">left</div>
+      <div class="middle">middle</div>
+      <div class="divider"></div>
+      <div class="flex-container-right">
+        <div class="week">
+          <img src="./images/arrow-left.svg" alt="" width="24" height="24">
+          <h3>This Week</h3>
+          <img src="./images/arrow-right.svg" alt="" width="24" height="24">
+        </div>
+        <div class="today">
+          <p>Today</p>
+          <div class="today-grid-cont">
+            <Today />
+            <Today />
+            <Today />
+            <Today />
+          </div>
+        </div>
+        <div class="tommorow-grid-cont">
+          <Tommorow />
+          <Tommorow />
+          <Tommorow />
+          <Tommorow />
+          <Tommorow />
+          <Tommorow />
+          <Tommorow />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style scoped lang="scss">
+.cont-main {
+  display: flex;
+  flex-direction: row;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+  color: #242424;
+  background-color: rgba(255, 255, 255, 0.87);
+
+  .divider {
+    background-color: gray;
+    height: 100vh;
+    width: 0.1vw;
+  }
+
+  .flex-container-right {
+    display: flex;
+    flex-direction: column;
+    width: 25vw;
+    height: 100vh;
+    padding: 0.5vw;
+    align-items: center;
+    row-gap: 2vh;
+
+    .week {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      height: 9.5%;
+    }
+
+    .today {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 25%;
+
+      p {
+        padding: 0vh 0vh 0vh 2.75vh;
+        font-size: 1rem;
+        height: 7vh;
+      }
+
+      .today-grid-cont {
+        display: grid;
+        grid-template-columns: 11vh 11vh 11vh 11vh;
+        justify-content: space-around;
+      }
+    }
+
+    .tommorow-grid-cont {
+      display: grid;
+      width: 95%;
+      grid-template-rows: 8vh 8vh 8vh 8vh 8vh 8vh 8vh;
+      row-gap: 1vh;
+    }
+  }
+
+  .cont-left {
+    margin: 0;
+    display: flex;
+    width: 20vw;
+    height: 100vh;
+  }
+
+  .middle {
+    margin: 0;
+    display: flex;
+    width: 54vw;
+    height: 100vh;
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+h3 {
+  margin: 0vw 4vw 0vw 4vw;
+  font-weight: 500;
+}</style>
+
---
+++ src/components/Tommorow.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="cont-i">
+    <div>
+      <p class="day"> Text </p>
+      <p class="date">date</p>
+    </div>
+    <div class="d2">
+      <p>20°</p>
+    </div>
+    <div><img src="src/images/sunY.svg" alt="" width="38" height="38"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cont-i {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.5vh 1.5vh;
+
+  .day {
+    font-size: 0.9rem;
+  }
+
+  .date {
+    font-size: 0.8rem;
+    color: gray;
+  }
+
+  .d2 {
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+}</style>
